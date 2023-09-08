920d0f8dcf5703b2d45b2130d37e85fdabd29869
Andrew Kimwetich <kimwetichandrew@gmail.com>
2023-09-08 10:02:17 +0300

    init

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,16 @@ import { UserRouter } from "./routers/usersrouter.js";
 import { profilesRouter } from "./routers/profiles.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", UserRouter);
 app.use("/profiles", profilesRouter);
+
+app.get("/trial", (req, res) => {
+  res.json({ message: "HEY AMIGO YOU HAVE REACHED US " });
+});
 
 const port = 5000;
 connectDb(() => {
